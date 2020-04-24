a8ef3c285806507e7358045cddeecc0ee1549db1
kristo palu <kristo.palu89@gmail.com>
2020-04-24 17:59:09 +0300

    add clear all fields/ui more responsive

--- src/index.js
+++ src/index.js
@@ -10,50 +10,32 @@ window.addEventListener("DOMContentLoaded", () => {
   let dataCut = dataString.replace(/ *\([^)]*\) */g, "");
   let dataFinal = JSON.parse(dataCut)
 
-
   //dice functsions
   const dice = () => {
-    //clear all 
 
-    console.log(mainElements.deathList)
-     for (let i=0;i < mainElements.lifeList.length;i++) { 
-       let index =  mainElements.lifeList[0]
-       index.innerHTML = ''
-    }
-
-    //Store key value pairs inside array
     let valueArray = []
+    //clear all 
+    for (let i = 0; i < 7; i++) {
+      mainElements.lifeList[i].innerHTML = `* * * * * *`
+      mainElements.deathList[i].innerHTML = `* * * * * *`
+    }
     //Get random object
     let randomObject = dataFinal.default[Math.floor(Math.random() * dataFinal.default.length)];
+    //add text into elements
     for (let [key, value] of Object.entries(randomObject)) {
-      //add text into elements
+      //Store key value pairs inside array
       valueArray.push((`${key}: ${value}`));
-      /*   mainElements.lifeList[0].innerHTML = `${valueArray[0]}`;
-        mainElements.lifeList[1].innerHTML = `${valueArray[1]}`;
-        mainElements.lifeList[2].innerHTML = `${valueArray[2]}`;
-        mainElements.lifeList[3].innerHTML = `${valueArray[3]}`;
-        mainElements.lifeList[4].innerHTML = `${valueArray[4]}`;
-        mainElements.lifeList[5].innerHTML = `${valueArray[5]}`;
-        mainElements.lifeList[6].innerHTML = `${valueArray[6]}`; */
+
       if (randomObject.d100 > 50) {
-        mainElements.deathList[0].innerHTML = `${valueArray[0]}`
-        mainElements.deathList[1].innerHTML = `${valueArray[1]}`
-        mainElements.deathList[2].innerHTML = `${valueArray[2]}`
-        mainElements.deathList[3].innerHTML = `${valueArray[3]}`
-        mainElements.deathList[4].innerHTML = `${valueArray[4]}`
-        mainElements.deathList[5].innerHTML = `${valueArray[5]}`
-        mainElements.deathList[6].innerHTML = `${valueArray[6]}`
+        for (let i = 0; i < 7; i++) {
+          mainElements.deathList[i].innerHTML = `${valueArray[i]}`
+        }
       } else {
-        mainElements.lifeList[0].innerHTML = `${valueArray[0]}`;
-        mainElements.lifeList[1].innerHTML = `${valueArray[1]}`;
-        mainElements.lifeList[2].innerHTML = `${valueArray[2]}`;
-        mainElements.lifeList[3].innerHTML = `${valueArray[3]}`;
-        mainElements.lifeList[4].innerHTML = `${valueArray[4]}`;
-        mainElements.lifeList[5].innerHTML = `${valueArray[5]}`;
-        mainElements.lifeList[6].innerHTML = `${valueArray[6]}`;
+        for (let i = 0; i < 7; i++) {
+          mainElements.lifeList[i].innerHTML = `${valueArray[i]}`
+        }
       }
     }
-    //create rows
   }
   //dice event
   mainElements.d100[0].addEventListener('click', dice);
